1d9abc58c82978ca580765eb9e7bdb5bca180572
kfernandezcr <107716734+kfernandezcr@users.noreply.github.com>
2022-06-17 13:55:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('New Healthy Diner')
+
+import.header('Breakfast Menu')
+
+import.text('Omega 3 & Blueberry Oatmeal')
+import.text('Kale, Spinach & Rocket Smoothie')
+import.text('Hard-Boiled Free-Range Egg')
